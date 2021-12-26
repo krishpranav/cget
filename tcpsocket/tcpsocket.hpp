39979829554835d7d3b1b7da9fe1d18a824c1867
krishpranav <krisna.pranav@gmail.com>
2021-12-26 14:04:17 +0530

    tcpsocket: tcpsocket hpp

--- tcpsocket/tcpsocket.hpp
+++ tcpsocket/tcpsocket.hpp
@@ -18,18 +18,29 @@
 
 namespace Sockets {
     using SocketDescriptor = int;
-
+    
     class TcpSocket {
     public:
         TcpSocket(int family, int flags);
         virtual ~TcpSocket();
-
+        
         TcpSocket(const TcpSocket &socket) = delete;
         TcpSocket& operator=(const TcpSocket &socket) = delete;
-
+        
         void connect(std::string adress, int port);
-
+        
         void send(const char *data, unsigned int length, int flags);
         long receive(char* msg, int len, int flags);
+        
+    private:
+        struct CloseSocketDescriptor
+        {
+            void operator()(SocketDescriptor* socket_desc_) const;
+        };
+        
+        struct sockaddr_in sockaddr_in_;
+        
+        std::unique_ptr<SocketDescriptor, CloseSocketDescriptor> socket_desc_ptr_;
+        std::unique_ptr<addrinfo, decltype(&freeaddrinfo)> addr_info_ptr_;
     };
 }
