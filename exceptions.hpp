1d23c05c1de2e16ea5409e00588bf4b553ff01f2
krishpranav <krisna.pranav@gmail.com>
2021-12-26 13:49:40 +0530

    exceptions: http protocol exception + wgetexceptio

--- exceptions.hpp
+++ exceptions.hpp
@@ -2,10 +2,19 @@
 
 #include <iostream>
 
-class SocketException : public std::runtime_error
-{
+class SocketException : public std::runtime_error {
 public:
     explicit SocketException(const std::string& message)
         : std::runtime_error{"[Socket] " + message}
     {}
+};
+
+class HttpProtocolException : public std::runtime_error {
+public:
+
+};
+
+class WgetException : public std::runtime_error {
+public:
+
 };
