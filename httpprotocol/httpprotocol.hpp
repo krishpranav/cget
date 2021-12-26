d84d3ac683550f35444f892873fb03f7e8b1ed26
krishpranav <krisna.pranav@gmail.com>
2021-12-26 14:21:18 +0530

    httpprotocol: httpprotocol.hpp

--- httpprotocol/httpprotocol.hpp
+++ httpprotocol/httpprotocol.hpp
@@ -15,34 +15,41 @@
 #include <regex>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 namespace Network {
-    class HttpProtcol : public Protocol {
+
+    class HttpProtocol : public Protocol {
     public:
         using Domain = Protocol::Host;
-        using Path = std::string;
-
-        HttpProtcol();
-        ~HttpProtcol() = default;
-
+        using Path   = std::string;
+        
+        HttpProtocol();
+        ~HttpProtocol() = default;
+        
         const Host GetHost() const override;
         void SetHost(const Host& host) override;
-
+        
         const Port GetPort() const override;
         void SetPort(const Port& port) override;
-
+        
         void SendRequest(const Path& path) const override;
         void ProcessResponse(std::ofstream& out_stream) const override;
-
+        
     private:
         Protocol::Host host_;
         Protocol::Port port_;
         Path path_;
-
-        std::unique_ptr<Sockets::TcpSocket> socket_ptr;
-    
+        
+        std::unique_ptr<Sockets::TcpSocket> socket_ptr_;
+        
     public:
         const static Protocol::Port default_http_port;
         const static size_t buffer_size_;
     };
+    
+    namespace Utils {
+        std::pair<std::string, std::string> parseUrl(const std::string& url);
+        const static std::regex url_regex{"http://([a-z-.]*)/(.*)"};
+    }
 }
