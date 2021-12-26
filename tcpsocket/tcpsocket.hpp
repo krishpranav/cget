f13f83e38c49ced4c7ec51c9b127cfd4afb3f0f0
krishpranav <krisna.pranav@gmail.com>
2021-12-26 14:00:10 +0530

    tcpsocket: tcpsocket hpp

--- tcpsocket/tcpsocket.hpp
+++ tcpsocket/tcpsocket.hpp
@@ -7,3 +7,29 @@
  * @copyright Copyright (c) 2021 Apache-2.0
  * 
  */
+
+#pragma once
+
+#include <string>
+#include <memory>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+namespace Sockets {
+    using SocketDescriptor = int;
+
+    class TcpSocket {
+    public:
+        TcpSocket(int family, int flags);
+        virtual ~TcpSocket();
+
+        TcpSocket(const TcpSocket &socket) = delete;
+        TcpSocket& operator=(const TcpSocket &socket) = delete;
+
+        void connect(std::string adress, int port);
+
+        void send(const char *data, unsigned int length, int flags);
+        long receive(char* msg, int len, int flags);
+    };
+}
