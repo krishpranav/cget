e9a243346ec7743f8d3af4277ef14bac0b174135
krishpranav <krisna.pranav@gmail.com>
2021-12-26 14:15:21 +0530

    tcpsocket: licensing

--- tcpsocket/tcpsocket.cpp
+++ tcpsocket/tcpsocket.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file tcpsocket.hpp
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2021-12-26
+ * 
+ * @copyright Copyright (c) 2021 Apache-2.0
+ * 
+ */
+
 #include "tcpsocket.hpp"
 #include "exceptions.hpp"
 #include <unistd.h>
