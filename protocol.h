372bcd507996465c63b8d1ff29f0da45f2347f26
krishpranav <krisna.pranav@gmail.com>
2021-12-26 13:55:53 +0530

    protocol header

--- protocol.h
+++ protocol.h
@@ -1,3 +1,13 @@
+/**
+ * @file protocol.h
+ * @author Krisna Pranav
+ * @version 0.1
+ * @date 2021-12-26
+ * 
+ * @copyright Copyright (c) 2021 Apache-2.0
+ * 
+ */
+
 #pragma once
 
 #include <string>
@@ -8,8 +18,16 @@ namespace Network {
         using Host = std::string;
         using Path = std::string;
         using Port = int;
-
+        
         virtual const Host GetHost() const = 0;
         virtual void SetHost(const Host& host) = 0;
+        
+        virtual const Port GetPort() const = 0;
+        virtual void SetPort(const Port& port) = 0;
+        
+        virtual void SendRequest(const Path& path) const = 0;
+        virtual void ProcessResponse(std::ofstream& out_stream) const = 0;
+        
+        virtual ~Protocol() {};
     };
 }
