75b06f15c899b1603a8642c74eb8486a4d8c5183
krishpranav <krisna.pranav@gmail.com>
2021-12-27 08:46:54 +0530

    wget: wget hpp publics

--- wget/wget.hpp
+++ wget/wget.hpp
@@ -8,3 +8,28 @@
  * @copyright Copyright (c) 2021 Apache-2.0
  * 
  */
+
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <regex>
+#include "tcpsocket.hpp"
+#include "protocol.h"
+#include <sstream>
+
+class Wget {
+public:
+    enum class ProtocolType
+    {
+        HTTP
+    };
+
+    Wget(ProtocolType protocolType);
+    ~Wget() = default;
+
+    Wget(const Wget &wget) = delete;
+    Wget& operator=(const Wget &wget) = delete;
+
+    std::string Download(const std::string& url);
+};
