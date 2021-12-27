c1e9b60d960972dc9c8680f308b468e3ad0ec212
krishpranav <krisna.pranav@gmail.com>
2021-12-27 08:47:54 +0530

    wget: wget hpp

--- wget/wget.hpp
+++ wget/wget.hpp
@@ -19,17 +19,25 @@
 #include <sstream>
 
 class Wget {
-public:
+public:    
     enum class ProtocolType
     {
         HTTP
     };
-
+    
     Wget(ProtocolType protocolType);
     ~Wget() = default;
-
+    
     Wget(const Wget &wget) = delete;
     Wget& operator=(const Wget &wget) = delete;
-
+    
     std::string Download(const std::string& url);
+    
+    
+private:
+    std::unique_ptr<Network::Protocol> http_;
+    std::unique_ptr<Network::Protocol> BuildProtocol(Wget::ProtocolType protocolType) const;
+    
+    const std::string GetOutputFilename(const std::string& path) const;
+    const std::string GetOutputFilePath(const std::string& filename) const;
 };
