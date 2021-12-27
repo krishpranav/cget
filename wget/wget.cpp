86f48d3c6b4e9d1983dd466ca8626c7338fe62bb
krishpranav <krisna.pranav@gmail.com>
2021-12-27 08:53:14 +0530

    wget: wget download

--- wget/wget.cpp
+++ wget/wget.cpp
@@ -8,3 +8,44 @@
  * @copyright Copyright (c) 2021 Apache-2.0
  * 
  */
+
+#include "wget.hpp"
+#include "httpprotocol.hpp"
+#include "exceptions.hpp"
+#include <unistd.h>
+
+Wget::Wget(ProtocolType protocolType)
+    : http_{BuildProtocol(protocolType)}
+{
+}
+
+std::string Wget::Download(const std::string& url)
+{
+    const auto& [domain, path] = Network::Utils::parseUrl(url);
+
+    http_->SetHost(domain);
+    http_->SetPort(Network::HttpProtocol::default_http_port);
+    http_->SendRequest(path);
+
+    const std::string& outfliename = GetOutputFilename(path);
+    std::ofstream outfile;
+    outfile.open (outfilename);
+
+    if (!outfile.is_open())
+    {
+        throw WgetException{"Can't create output file"};
+    }
+
+    try 
+    {
+        http_->ProcessResponse(outfile);
+    } catch(...)
+    {
+        outfile.close();
+        throw;
+    }
+
+    outfile.close()
+
+    return GetOutputFilePath(outfliename);
+};
